7b6577690ceff7c5709d73735a6baa608de54c95
MWI-Leb <jaymz.moore@amerisourcebergen.com>
2024-11-19 00:31:09 +0000

    Complete

--- src/Ch02/02_11b/CodeDemo.cpp
+++ src/Ch02/02_11b/CodeDemo.cpp
@@ -5,8 +5,23 @@
 #include <iostream>
 #include <cstdint>
 
-int main(){
-    
+int main()
+{
+    int farhenheit = 100;
+    int celcius;
+
+    celcius = ((float)5 / 9.0) * (farhenheit - 32);
+
+    std::cout << "Farenheit: " << farhenheit << std::endl;
+    std::cout << "Celcius: " << celcius << std::endl;
+
+    float weight = 10.99;
+
+    std::cout << std::endl;
+    std::cout << "Float:" << weight << std::endl;
+    std::cout << "Interger Part:" << (int) weight << std::endl;
+    std::cout << "Fractional Part:" << (double)((weight -(int) weight) * 10000) << std::endl;
+
     std::cout << std::endl << std::endl;
     return (0);
 }
