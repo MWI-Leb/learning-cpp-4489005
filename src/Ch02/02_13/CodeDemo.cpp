7e9be510032bf1ba4322925a2a42ed6113652622
MWI-Leb <jaymz.moore@amerisourcebergen.com>
2024-11-20 22:53:15 +0000

    complete

--- src/Ch02/02_13/CodeDemo.cpp
+++ src/Ch02/02_13/CodeDemo.cpp
@@ -6,11 +6,14 @@
 
 int main(){
     int nums[5] = {1,23,32,24,337};
-    float result; 
+    float result;
+    float print_result; 
 
     // Write your code here
+    result = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+    result /=5;
     
-    std::cout << "The average is " << result << std::endl;
+    std::cout << "The average is " <<result << std::endl;
     
     std::cout << std::endl << std::endl;
     return (0);
