4fe741679489adcf6d156054c1f6d074a0c380a6
MWI-Leb <jaymz.moore@amerisourcebergen.com>
2024-11-26 22:22:27 +0000

    complete

--- src/Ch03/03_11/CodeDemo.cpp
+++ src/Ch03/03_11/CodeDemo.cpp
@@ -5,6 +5,74 @@
 #include <iostream>
 #include "records.h"
 
+
+class student{
+public:
+    student(std::string name_i, int id_i){
+        name = name_i;
+        id = id_i;
+    }
+    std:: string get_name()const{
+        return name;
+    }
+    int get_id()const{
+        return id;
+    }
+
+private:
+    std::string name;
+    int id;
+    
+
+};
+
+class course{
+ course(std::string name_i, int id_i, int credits_i){
+        name = name_i;
+        id = id_i;
+        credits = credits_i;
+    }
+    std:: string get_name()const{
+        return name;
+    }
+    int get_id()const{
+        return id;
+    }
+    int get_credits()const{
+        return credits;
+    }
+
+private:
+    std::string name;
+    int id;
+    int credits;
+
+};
+
+class grades{
+   grades(int student_id_i, int course_id_i, char grade_i){
+        student_id = student_id_i;
+        course_id = course_id_i;
+        grade = grade_i;
+    }
+    int get_student_id()const{
+        return student_id;
+    }
+    int get_course_id()const{
+        return course_id;
+    }
+    char get_grade()const{
+        return grade;
+    }
+
+private:
+    int student_id;
+    int course_id;
+    char grade;
+
+};
+
+
 int main(){
     Student my_student(1, "Hilda Jones");
     Course my_course(7, "Physics 101", 4);
